9eb2ec374bdd0ac41c1465dc63fbddddfcaf65fd
Rui Ma <535657150@qq.com>
2023-12-13 17:20:29 +0800

    移除测试内容

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,7 @@ import { RouterView } from 'vue-router'
 
 <template>
   <transition name="el-fade-in">
-    <Suspense>
-      <template #default>
-        <RouterView />
-      </template>
-      <template #fallback>
-        <div class="fixed top-0 bottom-0 left-0 right-0 bg-red-500"></div>
-      </template>
-    </Suspense>
+    <RouterView />
   </transition>
 </template>
 
